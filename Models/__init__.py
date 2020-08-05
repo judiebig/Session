fa6528e1ec5f71152bee37b22370328a3ef393b3
JudieDai <870869788@qq.com>
2020-08-05 17:35:01 +0800

    implement the RepeatNet based on Re-GNN, under the same parameter setting , the result is 71.05 31.52

--- Models/__init__.py
+++ Models/__init__.py
@@ -1,5 +1,4 @@
 # from Models.sr_iem import *
 # from Models.sr_iem_pos import *
-from Models.sr_iem_pos_cl import *
-# from Models.sr_iem_pos_cl_2 import *
-# from Models.sr_iem_pos_cl_1_att import *
+# from Models.sr_iem_pos_cl import *
+from Models.repeat_net import *
